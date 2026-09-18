5b76861def67a3d569011ca0538025c4c3e789e1
Marlon <mbbrandl09@gmail.com>
2026-09-18 15:44:45 +0200

    Update password_safe.py
    FINAL update to my learning project

--- password_safe.py
+++ password_safe.py
@@ -1,4 +1,4 @@
-import random   #für password generator
+import secrets  #für password generator
 import string   #um possible characters einfacher darzustellen
 import json     #speichern und laden meiner datei wo alles gespeichert wird
 from cryptography.fernet import Fernet #zum encrypten/decrypten der passwörter
@@ -59,7 +59,7 @@ def decrypt_password(encrypted_password):
 
 # Password generator für reines Password generieren
 def password_generator(min_char=8, max_chars=20):
-    password = "".join(random.choice(possible_chars) for _ in range(random.randint(min_char, max_chars)))
+    password = "".join(secrets.choice(possible_chars) for _ in range(secrets.randbelow(max_chars -min_char + 1) + min_char))
     return password
 
 #print(password_generator())
@@ -72,14 +72,29 @@ def is_password_valid(password, min_length=8):
         return False
     return True
 
+
+def show_password_conditions():
+    return """
+Password Conditions:
+
+- Only upper- and lowercase letters
+- 0-9 digits
+- !+-.,#?$%* are allowed
+- min. 8 characters
+"""
+
+    
 #Hinzufügen von einem Password
 def create_password():
 
-    overwrite = False    
+    if "show_overwrite" not in st.session_state:
+        st.session_state.show_overwrite = False
+
     service = st.text_input("Service(casesensitive): ").strip()
     username = st.text_input("Username/Email:").strip()
     password = st.text_input("Password:", type="password").strip()
     password_confirm = st.text_input("Confirm password:", type="password").strip()
+
     if st.button("Save:"):
         if service == "":
             st.error("Service can't be empty!")
@@ -89,32 +104,32 @@ def create_password():
             st.error("Password can't be empty!")
         elif not is_password_valid(password):
             st.error("The password isn't valid.")
-            st.text("""
-Password Conditions:
-
-- Only upper- and lowercase letters
-- 0-9 digits
-- !+-.,#?$%* are allowed
-- min. 8 characters
-
-            """)
+            st.text(show_password_conditions())
         elif password != password_confirm:
             st.error("The passwords don't match!")
         elif service in safe:
             st.warning(f"An entry for '{service}' already exists.")
-            overwrite = st.checkbox("Overwrite existing entry?")
+            st.session_state.show_overwrite = True
         else:
-            overwrite = True
+            safe[service] = { "username": username, "password": encrypt_password(password)}
+            save_to_file()
+            st.success("Successfully saved!")
 
-        if overwrite == True:
-                safe[service] = {"username": username, "password": encrypt_password(password)}
-                save_to_file()
-                st.success("Succesfully saved!")
+    if st.session_state.show_overwrite:
+        overwrite = st.checkbox("Overwrite existing entry?")
+
+        if overwrite:
+            safe[service] = {"username": username, "password": encrypt_password(password)}
+            save_to_file()
+            st.success("Successfully overwritten!")
+            st.session_state.show_overwrite = False
+            st.rerun()
 
 
 
 def dashboard():
-    caption = False
+    if "show_password" not in st.session_state:
+        st.session_state.show_password = None
     if "delete_confirm" not in st.session_state:
         st.session_state.delete_confirm = None
     if "edit_username" not in st.session_state:
@@ -122,16 +137,17 @@ def dashboard():
     if "edit_password" not in st.session_state:
             st.session_state.edit_password = None
     for i in safe:
+        caption = False
         with st.expander(i):
             col1, col2, col3 = st.columns([2, 3, 1], vertical_alignment="center")
             with col1:
                 st.write("Username/Email:")
             with col2:
                 if st.session_state.edit_username == i:
-                    username = st.text_input("New username:", value=safe[i]["username"])
+                    username = st.text_input("New username:", value=safe[i]["username"], key=f"btw_new_username{i}")
                     col4, col5 = st.columns([1,2])
                     with col4:
-                        if st.button("Save", key="btn_username"):
+                        if st.button("Save", key=f"btn_username{i}"):
                             if not username == "":
                                 safe[i]["username"] = username
                                 save_to_file()
@@ -158,22 +174,14 @@ def dashboard():
                 st.write("Password:")
             with col2:
                 if st.session_state.edit_password == i:
-                    password = st.text_input("New password:", type="password", value=decrypt_password(safe[i]["password"])) 
+                    password = st.text_input("New password:", type="password", value=decrypt_password(safe[i]["password"]), key =f"btn_new_password{i}") 
                     col4,col5 = st.columns([1,2])
                     with col4:
-                        if st.button("Save", key= f"btn_password{safe[i]}"):
+                        if st.button("Save", key= f"btn_password{i}"):
                             if not is_password_valid(password):
                                 with col5:
-                                    if not is_password_valid(password):
-                                        st.caption("The password isn't valid.")
-                                        st.text("""
-                                Password Conditions:
-                                
-                                - Only upper- and lowercase letters
-                                - 0-9 digits
-                                - !+-.,#?$%* are allowed
-                                - min. 8 characters
-                                            """)
+                                    st.caption("The password isn't valid.")
+                                    st.text(show_password_conditions())
                             else:
                                 safe[i]["password"] = encrypt_password(password)
                                 save_to_file()
@@ -181,7 +189,11 @@ def dashboard():
                                 st.rerun()
                     
                 else:
-                    st.markdown(f"`{decrypt_password(safe[i]['password'])}`")
+                    if st.session_state.show_password == i:
+                        st.markdown(f"`{decrypt_password(safe[i]["password"])}`")
+                    else:
+                        password_length = "*" * len(decrypt_password(safe[i]["password"]))
+                        st.markdown(f"`{password_length}`")
             with col3:
                 is_editing = st.session_state.edit_password == i
                 buttonchange = "Cancel" if is_editing else "Change"
@@ -193,9 +205,9 @@ def dashboard():
                     st.rerun()
                         
 #deletion teil
-            col1,col2= st.columns([1,5], vertical_alignment="center")
+            col1,col2,col3= st.columns([1,5,2], vertical_alignment="center")
             with col1:
-                if st.button("Delete", key=f"btn_deletion{safe[i]}"):
+                if st.button("Delete", key=f"btn_deletion{i}"):
                     if st.session_state.delete_confirm == i:
                         del safe[i]
                         save_to_file()
@@ -205,15 +217,19 @@ def dashboard():
                         caption = True
 
             with col2:
-                if st.checkbox("Confirm deletion", key=f"check_deletion{safe[i]}"):
+                if st.checkbox("Confirm deletion", key=f"check_deletion{i}"):
                     st.session_state.delete_confirm = i
+
+            with col3:
+                if st.button("Show", key=f"btn_show{i}"):
+                    if st.session_state.show_password == i:
+                        st.session_state.show_password = None
+                    else:    
+                        st.session_state.show_password = i
+                    st.rerun()
 
             if caption == True:
                 st.caption("Please check the textbox on the right.")
-
-
-
-    save_to_file()
 
 
 
